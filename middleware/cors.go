d19929bf63ffa5567bca8ce3cb476c3ee2670b8f
JokerTrickster <pkjhj485@gmail.com>
2025-01-19 18:55:55 +0900

    {feat} - cors 미들웨어 추가 \n

--- middleware/cors.go
+++ middleware/cors.go
@@ -1,1 +1,24 @@
 package middleware
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+// CORSConfig defines the default configuration for CORS middleware
+func CORSConfig() echo.MiddlewareFunc {
+	return middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"}, // 허용할 도메인. "*"는 모든 도메인 허용
+		AllowMethods: []string{
+			http.MethodGet,
+			http.MethodPost,
+			http.MethodPut,
+			http.MethodDelete,
+			http.MethodOptions,
+			http.MethodPatch,
+			http.MethodHead,
+		}, // 허용할 HTTP 메서드
+	})
+}
